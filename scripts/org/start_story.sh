d30011409c16fd7c3f37478f7ca5e55eba6081b8
baw <baw@mw2TnKwq.local>
2025-11-15 22:17:58 -0500

    chore: fix start_story.sh

--- scripts/org/start_story.sh
+++ scripts/org/start_story.sh
@@ -4,7 +4,7 @@ BRANCH=${1:-"feat/$(date +%Y%m%d-%H%M%S)"}
 echo "Creating branch $BRANCH"
 git checkout -b "$BRANCH"
 ALIAS="so-$(date +%s)"
-sf org create scratch -f config/project-scratch-def.json -d 7 -a "$ALIAS"
+sf org create scratch -f config/project-scratch-def.json --duration-days 7 --alias "$ALIAS"
 sf project deploy start -o "$ALIAS"
 sf org open -o "$ALIAS"
 echo "Scratch org alias: $ALIAS"
